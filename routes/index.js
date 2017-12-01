e82bc900ca678e662428bfeef94b242cd3c1eb27
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-01 09:05:01 -0600

    + customer experience demo

--- routes/index.js
+++ routes/index.js
@@ -49,15 +49,21 @@ router.get('/demos', function (req, res, next) {
   var workshops = [
     {
       name: 'Ducati: Extend CRM',
-      description: 'Custom Objects help manage Bikes, Dealers, and Live Events.  A Lightning App built with Components helps organize Live Events and keep Sales Managers in the loop.  (From the DF17 Campground)',
+      description: 'Custom Objects help manage Bikes, Dealers, and Live Events.  A Lightning App built with Components helps organize Live Events and Process Builder keep Sales Managers in the loop.  (from the DF17 Campground)',
       launchURL: launcher + 'https://github.com/mshanemc/cg1',
       docsURL: 'https://salesforce.quip.com/bY08A4XodAFN'
     },
     {
       name: 'Ducati: Integrate Data from Anywhere',
-      description: 'Salesorce Connect brings Invoices from an ERP system into Salesforce, including the mobile app, with no code.  (From the DF17 Campground)',
+      description: 'Salesorce Connect brings Invoices from an ERP system into Salesforce, including the mobile app, with no code.  (from the DF17 Campground)',
       launchURL: launcher + 'https://github.com/mshanemc/cg4Integrate',
       docsURL: 'https://salesforce.quip.com/sh0EAdWZDsiW'
+    },
+    {
+      name: 'Ducati: Transform Customer Experiences',
+      description: 'Our broad vision--Connected products, customer-facing mobile apps backed by Heroku + Heroku Pipelines, and Lightning seamlessly share information with Heroku Connect and Platform Events.  Some setup required, but totally worth it! (from the DF17 Campground)',
+      launchURL: launcher + 'https://github.com/mshanemc/platform-campground-customer-exp',
+      docsURL: 'https://salesforce.quip.com/pySzAu1av18a'
     }
   ];
   res.render('demos', { workshops: workshops });
