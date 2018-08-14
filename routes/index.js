0536b8e102d90935dfd0b991fb84baae3b6eeb5b
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-14 11:25:38 -0500

    no logs

--- routes/index.js
+++ routes/index.js
@@ -9,38 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:mix', function (req, res, next) {
-  console.log(req.params.mix);
   res.render('index', { workshops: workshops.getDefs(workshops[req.params.mix]) })
 });
-
-// router.get('/daydreamin', function (req, res, next) {
-//   res.render('index', { workshops: workshops.getDefs(workshops.daydreamin) })
-// });
-
-// router.get('/df18zone', function (req, res, next) {
-//   res.render('index', { workshops: workshops.getDefs(workshops.df18zone) });
-// });
-
-// router.get('/df18rapids', function (req, res, next) {
-//   res.render('index', { workshops: workshops.getDefs(workshops.df18rapids) });
-// });
-
-// router.get('/df18adoption', function (req, res, next) {
-//   res.render('index', { workshops: workshops.getDefs(workshops.df18adoption) });
-// });
-
-// router.get('/adoption', function (req, res, next) {
-//   res.render('index', { workshops: workshops.getDefs(workshops.adoption) });
-// });
-
-// router.get('/demos', function (req, res, next) {
-//   res.render('demos', { workshops: workshops.getDefs(workshops.demos) });
-// });
-
-// router.get('/df18campground', function (req, res, next) {
-//   res.render('demos', { workshops: workshops.getDefs(workshops.df18campground) });
-// });
-
-
 
 module.exports = router;
