440870765dfd31614cb80e13dd445caf3e93dcc5
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-14 11:09:45 -0500

    parameterized dynamic routes

--- routes/index.js
+++ routes/index.js
@@ -8,28 +8,38 @@ router.get('/', function(req, res, next) {
   res.render('index', { workshops: workshops.getDefs(workshops.main)});
 });
 
-router.get('/daydreamin', function (req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops.daydreamin) })
+router.get('/:mix', function (req, res, next) {
+  res.render('index', { workshops: workshops.getDefs(workshops[req.param.mix]) })
 });
 
-router.get('/df18zone', function (req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops.df18zone) });
-});
+// router.get('/daydreamin', function (req, res, next) {
+//   res.render('index', { workshops: workshops.getDefs(workshops.daydreamin) })
+// });
 
-router.get('/df18rapids', function (req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops.df18rapids) });
-});
+// router.get('/df18zone', function (req, res, next) {
+//   res.render('index', { workshops: workshops.getDefs(workshops.df18zone) });
+// });
 
-router.get('/df18adoption', function (req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops.df18adoption) });
-});
+// router.get('/df18rapids', function (req, res, next) {
+//   res.render('index', { workshops: workshops.getDefs(workshops.df18rapids) });
+// });
 
-router.get('/adoption', function (req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops.adoption) });
-});
+// router.get('/df18adoption', function (req, res, next) {
+//   res.render('index', { workshops: workshops.getDefs(workshops.df18adoption) });
+// });
 
-router.get('/demos', function (req, res, next) {
-  res.render('demos', { workshops: workshops.getDefs(workshops.demos) });
-});
+// router.get('/adoption', function (req, res, next) {
+//   res.render('index', { workshops: workshops.getDefs(workshops.adoption) });
+// });
+
+// router.get('/demos', function (req, res, next) {
+//   res.render('demos', { workshops: workshops.getDefs(workshops.demos) });
+// });
+
+// router.get('/df18campground', function (req, res, next) {
+//   res.render('demos', { workshops: workshops.getDefs(workshops.df18campground) });
+// });
+
+
 
 module.exports = router;
