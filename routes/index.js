5d76179acf4a41fd35d202ccc650a4766ca8a437
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-16 11:32:56 -0500

    speical pages for adoption

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/:mix', function (req, res, next) {
   if (workshops[req.params.mix]){
-    res.render('index', { workshops: workshops.getDefs(workshops[req.params.mix]) });
+    // default page
+    let page = 'index';
+
+    // special cases for adoption pages, which have different treatment
+    if (req.params.mix === 'adoption' || req.params.mix === 'df18adoption'){
+      page = 'adoption';
+    }
+
+    res.render(page, { workshops: workshops.getDefs(workshops[req.params.mix]) });
+
   } else {
     res.render('error', { message: `Not found: ${req.params.mix}`, error : { status: 404}});
   }
